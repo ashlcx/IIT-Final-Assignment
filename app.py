fc34e77c46c3e601fab0d3809b8b386d5495e4b3
Ash Hines <ashhines95@gmail.com>
2021-05-24 12:28:20 +1000

    Removed unused imports

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-from logging import exception
 import sys
 
 ## Cant use tuple or array as negative will call nth value
